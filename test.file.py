ce0f588cf0f04ef7d66dfbdf54f48db517548587
2218054hwangjihwan <144421318+2218054hwangjihwan@users.noreply.github.com>
2023-09-08 16:25:31 +0900

    2218054황지환 퀴즈제출
     9.8일 컴프 실습 내용제출입니다

--- test.file.py
+++ test.file.py
@@ -1,1 +1,22 @@
-print("hello world")
+number = input("숫자를 입력하세요")
+print(number)
+
+
+name = input("이름을 입력하세요")
+age = input("나이를 입력하세요")
+print("이름은",name,"나이는",age)
+
+
+a=input("숫자를 입력하세요")
+b=input("숫자를 입력하세요")
+print (int(a)*int(b))
+
+
+두숫자는 각각 a,b 입니다
+
+
+name=input("이름을 입력하세요")
+a=input("숫자를 입력하세요")
+b=input("숫자를 입력하세요")
+
+print("안녕하세요",name,"두숫자는 각각",a,b)
